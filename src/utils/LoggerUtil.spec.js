14cb49f36d916fc1dfd1b03e5b1a092eeec9bd9b
akirsanov <Alexander.Kirsanov@reltio.com>
2015-10-27 17:50:59 +0300

    Logger utils tests

--- src/utils/LoggerUtil.spec.js
+++ src/utils/LoggerUtil.spec.js
@@ -1,4 +1,5 @@
 import LoggerUtil from './LoggerUtil.js';
+import Levels from '../Levels.js';
 
 describe('LoggerUtil', function () {
     it('Should be defined', function () {
@@ -15,9 +16,32 @@ describe('LoggerUtil', function () {
             expect(LoggerUtil.getParentNames('siel.utils.Logger')).toEqual(['root', 'siel', 'siel.utils']);
         });
         it('Should take the parent names from cache', function () {
-            let a = LoggerUtil.getParentNames('siel.utils.Logger');
-            let b = LoggerUtil.getParentNames('siel.utils.Logger');
+            const a = LoggerUtil.getParentNames('siel.utils.Logger');
+            const b = LoggerUtil.getParentNames('siel.utils.Logger');
             expect(a === b).toBeTruthy();
+        });
+    });
+    describe('Record creation verification', function () {
+        const name = 'name';
+        const levelName = 'INFO';
+        const message = 'message';
+        const args = ['test'];
+        it('Should return message object', function () {
+            let record = LoggerUtil.makeRecord(name, levelName, message, args);
+            expect(record.level).toBe(Levels.getLevel(levelName));
+            expect(record.name).toBe(name);
+            expect(record.message).toBe(message);
+            expect(record.timestamp).toBeDefined();
+            expect(record.args).toEqual(args);
+        });
+        it('Should set the current time as timestamp', function () {
+            jasmine.clock().install();
+            jasmine.clock().mockDate();
+            let record = LoggerUtil.makeRecord(name, levelName, message, args);
+            jasmine.clock().tick(5000);
+            let record2 = LoggerUtil.makeRecord(name, levelName, message, args);
+            jasmine.clock().uninstall();
+            expect(record.timestamp).toBeLessThan(record2.timestamp);
         });
     });
 });
