5916cb341751a5c332b440ebd91b08a6c5914e93
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-10-29 23:19:42 +0300

    lint fixes

--- src/filters/Filters.spec.js
+++ src/filters/Filters.spec.js
@@ -8,41 +8,53 @@ describe('Filters', () => {
     beforeEach(() => {
         filters = new Filters();
     });
-    describe('Add filter verification', function(){
+    describe('Add filter verification', function () {
         it('Should save all filters in store', () => {
-            filters.addFilter(()=>{});
-            filters.addFilter(()=>{});
+            let fn = ()=> {
+            };
+            filters.addFilter(fn);
+            filters.addFilter(fn);
             expect(filters.getFilters().length).toBe(2);
         });
     });
-    describe('Remove filter verification', function(){
+    describe('Remove filter verification', function () {
         it('Should remove filter from store', () => {
-            let fn = ()=>{};
+            let fn = ()=> {
+            };
             filters.addFilter(fn);
             expect(filters.getFilters().length).toBe(1);
             filters.removeFilter(fn);
             expect(filters.getFilters().length).toBe(0);
         });
         it('Shouldn\'t remove non existing filter', () => {
-            let fn = ()=>{};
+            let fn = ()=> {
+            };
+            let fn2 = () => {
+            };
             filters.addFilter(fn);
             expect(filters.getFilters().length).toBe(1);
-            filters.removeFilter(() => {});
+            filters.removeFilter(fn2);
             expect(filters.getFilters().length).toBe(1);
         });
     });
 
-    describe('Filter verification', function(){
+    describe('Filter verification', function () {
+        let trueFn = ()=> {
+            return true;
+        };
+        let falseFn = ()=> {
+            return false;
+        };
         it('Should return false if at least one filter return false', () => {
-            filters.addFilter(()=>{return true;});
-            filters.addFilter(()=>{return true;});
-            filters.addFilter(()=>{return false;});
+            filters.addFilter(trueFn);
+            filters.addFilter(trueFn);
+            filters.addFilter(falseFn);
             expect(filters.filter()).toBeFalsy();
         });
         it('Should return true if all filters passes the test', () => {
             filters = new Filters();
-            filters.addFilter(()=>{return true;});
-            filters.addFilter(()=>{return true;});
+            filters.addFilter(trueFn);
+            filters.addFilter(trueFn);
             expect(filters.filter()).toBeTruthy();
         });
 
@@ -54,5 +66,5 @@ describe('Filters', () => {
             expect(test).toHaveBeenCalledWith('test');
         });
     });
-
 });
+
