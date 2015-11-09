56ba207217e7ff95ded85773078cc77d3fd919f0
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-11-09 22:52:44 +0300

    initial handler implementation

---
+++ src/handlers/Handler.js
@@ -0,0 +1,63 @@
+import Levels from '../Levels.js';
+
+class Handler {
+    static add(item) {
+        Handler.all = Handler.all || [];
+        Handler.all.push(item);
+        this.filters = new Filters();
+        this.formatter = new DefaultFormatter();
+    }
+
+    static removeAll() {
+        Handler.all = [];
+    }
+
+    constructor(options) {
+        if (typeof  options !== 'object') {
+            options = {level: options};
+        }
+        let level = options.level ? Levels.getLevel(options.level) : Levels.Level.NO;
+        this.setLevel(level);
+        if (options.formatter) {
+            this.setFormatter(options.formatter);
+        } else if (options.format) {
+            this.setFormatter(new DefaultFormatter(options.format));
+        }
+
+    }
+
+    getFilters() {
+        return this.filters;
+    }
+
+    iEmit(...args) {
+        return new Promise((resolve) => {
+            resolve(this.emit.apply(this, args));
+        });
+    }
+
+    handle(record) {
+        if (!this.filters.filter(record)) {
+            return Promise.fulfilled();
+        }
+        if (this.emit.length < 2) {
+            throw new Error('Handler.emit requires a callback argument');
+        }
+
+        return this.iEmit(record);
+    }
+
+    format(record) {
+        return this.formatter.format(record);
+    }
+
+    setFormatter(formatter) {
+        this.formatter = formatter;
+        return this;
+    }
+
+    setLevel(level) {
+        this.level = level;
+        return this;
+    }
+}
