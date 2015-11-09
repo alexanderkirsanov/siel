9e50cf3947b49d8fd866eb2f641acb226f637a7c
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-11-09 23:57:02 +0300

    console handler initial implementation

--- src/handlers/Console.js
+++ src/handlers/Console.js
@@ -1,3 +1,33 @@
-class  Console extends Handler{
+import Handler from './Handler.js';
+import Levels from '../Levels.js';
 
+class Console extends Handler {
+    emit(record) {
+        let message = this.format(record);
+        let Level = Levels.Level;
+        /*eslint-disable no-console*/
+        switch (this.level) {
+            case Level.DEBUG:
+                console.log(message);
+                break;
+            case Level.ERROR:
+                console.error(message);
+                break;
+            case Level.FATAL:
+                console.error(message);
+                break;
+            case Level.INFO:
+                console.info(message);
+                break;
+            case Level.VERBOSE:
+                console.log(message);
+                break;
+            case Level.WARNING:
+                console.warn(message);
+                break;
+            default :
+                break;
+        }
+        /*eslint-enable no-console*/
+    }
 }
