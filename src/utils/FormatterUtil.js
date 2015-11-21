a4e9f4cf9b4a96079de1e44e1b12892714b187f2
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-11-21 23:26:10 +0300

    correct format function implementation

--- src/utils/FormatterUtil.js
+++ src/utils/FormatterUtil.js
@@ -1,41 +1,36 @@
-class FormatterUtil{
-    static format(f){
+class FormatterUtil {
+    static format(pattern, ...params) {
         let formatRegExp = /%[sdj%]/g;
-            if (typeof f !== 'string') {
-                var objects = [];
 
-                for (let i = 0; i < arguments.length; i++) {
-                    objects.push(inspect(arguments[i]));
-                }
-                return objects.join(' ');
+        let i = 1;
+        let len = params.length;
+        let str = String(pattern).replace(formatRegExp, function (x) {
+            if (x === '%%') {
+                return '%';
             }
-
-            let i = 1;
-            var args = arguments;
-            var len = args.length;
-            var str = String(f).replace(formatRegExp, function(x) {
-                if (x === '%%') return '%';
-                if (i >= len) return x;
-                switch (x) {
-                    case '%s': return String(args[i++]);
-                    case '%d': return Number(args[i++]);
-                    case '%j':
-                        try {
-                            return JSON.stringify(args[i++]);
-                        } catch (_) {
-                            return '[Circular]';
-                        }
-                    default:
-                        return x;
-                }
-            });
-            for (var x = args[i]; i < len; x = args[++i]) {
-                if (isNull(x) || !isObject(x)) {
-                    str += ' ' + x;
-                } else {
-                    str += ' ' + inspect(x);
-                }
+            if (i >= len) {
+                return x;
             }
-            return str;
+            switch (x) {
+                case '%s':
+                    return String(params[i++]);
+                case '%d':
+                    return Number(params[i++]);
+                case '%j':
+                    try {
+                        return JSON.stringify(params[i++]);
+                    } catch (_) {
+                        return '[Circular]';
+                    }
+                default:
+                    return x;
+            }
+        });
+        for (let x = params[i]; i < len; x = args[++i]) {
+            if (x === null || typeof x !== 'object') {
+                str += ' ' + x;
+            }
+        }
+        return str;
     }
 }
